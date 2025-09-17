5f06f0fdae7c0c2f3ff32d2a3eb6dc12e6e85778
LucioArgiro <aleargiro3@gmail.com>
2025-09-17 19:42:50 -0300

    Configuración para deploy en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   // ✅ Esta es la línea clave
+  base: '/Ecommerce/', 
 })
